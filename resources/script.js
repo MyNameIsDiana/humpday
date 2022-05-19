c18e5410c56c20db7851207c0161dfd3280750fc
Diana <dianagithubdavis@gmail.com>
2022-05-19 01:18:46 -0400

    adds some ideas, start of outline

---
+++ resources/script.js
@@ -0,0 +1,75 @@
+const people = new function() {
+    //in HTML show list of people -- names and phone numbers -- who should receive texts
+    //read 
+    //need function here to grab all the names/numbers -- loop through array and display them
+    //maybe count them
+    //should this save/show the last few memes sent? that way it doesn't send the same meme over and over. maybe don't display the history on main page, but instead display if you click on the name
+    //display in HTML file
+
+    //create
+    //need function here to add new meme to the collection
+    //is there a way to check if the one being added is already included? not sure
+
+    //edit
+    //need a way to edit each person's display name and phone number
+
+    //delete
+    //need function here to remove meme from the collection
+}
+
+const camels = new function() {
+    //in HTML show all the camel memes I've added
+    this.item = document.getElementById('camel-memes');
+    //the array of images -- need to start with a few already in here
+    this.camel_memes = [];
+
+    //read 
+    //need function here to grab all the camel memes -- loop through camel_memes array and display them
+    //maybe count them
+    //display in HTML file the images and the count
+
+    //create
+    //need function here to add new meme to the collection
+    //is there a way to check if the one being added is already included? not sure
+
+    //edit
+    //don't think an edit option is needed
+
+    //delete
+    //need function here to remove meme from the collection
+}
+
+const mikes = new function(){
+    //in HTML show all the Mike memes I've added
+    this.item = document.getElementById('mike-memes');
+    //the array of images -- need to start with a few already in here
+    this.mike_memes = [];
+
+    //read 
+    //need function here to grab all the camel memes -- loop through camel_memes array and display them
+    //maybe count them
+    //display in HTML file the images and the count
+
+    //create
+    //need function here to add new meme to the collection
+    //is there a way to check if the one being added is already included? not sure
+
+    //edit
+    //don't think an edit option is needed
+
+    //delete
+    //need function here to remove meme from the collection
+}
+//need function to allow user to make random choice
+//maybe also display grid of memes so user can choose specific one -- radio buttons?
+
+//need way to send meme as text message
+
+
+//how will this display all the people who need to receive texts? maybe list:
+    // person name (no need to display phone number)
+    // camel or mike? click image to choose which kind of meme to send
+        //that click leads to display of all the memes in that category
+        //can click radio button to make choice OR can click pick-one button for random choice
+        
+
